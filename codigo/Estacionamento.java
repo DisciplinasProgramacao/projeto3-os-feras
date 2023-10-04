3df8b2cccf2a1411c626d5aef7181ae9bf5ddc1e
FelipeNeb <122832893+FelipeNeb@users.noreply.github.com>
2023-10-04 12:10:55 -0300

    Update Estacionamento.java

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,51 +1,54 @@
-
-
 public class Estacionamento {
 
-	private String nome;
-	private Cliente[] id;
-	private Vaga[] vagas;
-	private int quantFileiras;
-	private int vagasPorFileira;
+    private String nome;
+    private Cliente[] id;
+    private Vaga[] vagas;
+    private int quantFileiras;
+    private int vagasPorFileira;
 
-	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
-	}
+    public Estacionamento(String nome, int capacidadeMaximaClientes, Vaga[] vagas, int quantFileiras, int vagasPorFileira) {
+        this.nome = nome;
+        this.id = new Cliente[capacidadeMaximaClientes];
+        this.vagas = vagas;
+        this.quantFileiras = quantFileiras;
+        this.vagasPorFileira = vagasPorFileira;
+    }
 
-	public void addVeiculo(Veiculo veiculo, String idCli) {
-		
-	}
+    public void addVeiculo(Veiculo veiculo, String idCli) {
+    }
 
-	public void addCliente(Cliente cliente) {
-		
-	}
+    public void addCliente(Cliente cliente) {
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == null) {
+                id[i] = cliente;
+                return; 
+            }
+        }
+        System.out.println("A capacidade máxima de clientes foi atingida.");
+    }
 
-	private void gerarVagas() {
-		
-	}
+    public void addVagas(Vaga[] novasVagas) {
+        this.vagas = novasVagas;
+    }
 
-	public void estacionar(String placa) {
-		
-	}
+    private void gerarVagas() {
+    }
 
-	public double sair(String placa) {
-		
-	}
+    public void estacionar(String placa) {
+    }
 
-	public double totalArrecadado() {
-		
-	}
+    public double sair(String placa) {
+    }
 
-	public double arrecadacaoNoMes(int mes) {
-		
-	}
+    public double totalArrecadado() {
+    }
 
-	public double valorMedioPorUso() {
-		
-	}
+    public double arrecadacaoNoMes(int mes) {
+    }
 
-	public String top5Clientes(int mes) {
-		
-	}
+    public double valorMedioPorUso() {
+    }
 
+    public String top5Clientes(int mes) {
+    }
 }

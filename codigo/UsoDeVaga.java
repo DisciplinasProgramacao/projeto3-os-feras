9e4aba34e56ca4447f80bd985dafebcdd30606ab
SthelTorres <130579600+SthelTorres@users.noreply.github.com>
2023-10-04 12:06:16 -0300

    Criação de classe uso de vaga

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,4 +1,4 @@
-
+import java.time.LocalDateTime;
 
 public class UsoDeVaga {
 
@@ -10,16 +10,25 @@ public class UsoDeVaga {
 	private LocalDateTime saida;
 	private double valorPago;
 
+	//construtor
 	public UsoDeVaga(Vaga vaga) {
-		
-	}
+		this.vaga=vaga;
+		this.entrada=LocalDateTime.now();//atribuir data e hora atual como entrada do cliente na vaga
+		this.saida=null;
+		this.valorPago=0.0;
 
+	}
+//registrar saida e retornar ao valor pago 
 	public double sair() {
-		
+		this.saida=LocalDateTime.now();//atribuir data e hora atual como entrada do cliente na vaga
+		this.valorPago=valorPago();//calcular valor pagopelo cliente
+		vaga.setOcupada(false);//liberar vaga
+		return this.valorPago;
 	}
 
+	//retornar valor pago pelo uso da vaga
 	public double valorPago() {
-		
+		return this.valorPago;
 	}
 
 }

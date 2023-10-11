e18839072f09972913ebe073f01d8b59cc4a0788
joaovitorbarrel <130581220+joaovitorbarrel@users.noreply.github.com>
2023-10-11 11:35:48 -0300

    Update Veiculo.java

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,59 +1,30 @@
 public class Veiculo {
-    private String placa;
-    private UsodeVaga[] usos;
 
-    public Veiculo(String placa) {
-        this.placa = placa;
-        this.usos = new UsodeVaga[500];
-    }
+	private String placa;
+	private UsoDeVaga[] usos;
 
-    public void estacionar(Vaga vaga) {
-        for (int i = 0; i < usos.length; i++) {
-            if (usos[i] == null) {
-                usos[i] = new UsodeVaga(vaga);
-                break;
-            }
-        }
-    }
+	public Veiculo(String placa) {
 
-    public double sair() {
-        double total = 0;
-        for (int i = 0; i < usos.length; i++) {
-            if (usos[i] != null) {
-                total += usos[i].getVaga().getValorPago();
-                usos[i] = null;
-            }
-        }
-        return total;
-    }
+	}
 
-    public double totalArrecadado() {
-        double total = 0;
-        for (int i = 0; i < usos.length; i++) {
-            if (usos[i] != null) {
-                total += usos[i].getVaga().getValorPago();
-            }
-        }
-        return total;
-    }
+	public void estacionar(Vaga vaga) {
 
-    public double arrecadadoNoMes(int mes) {
-        double total = 0;
-        for (int i = 0; i < usos.length; i++) {
-            if (usos[i] != null && usos[i].getVaga().getMes() == mes) {
-                total += usos[i].getVaga().getValorPago();
-            }
-        }
-        return total;
-    }
+	}
 
-    public int totalDeUsos() {
-        int total = 0;
-        for (int i = 0; i < usos.length; i++) {
-            if (usos[i] != null) {
-                total++;
-            }
-        }
-        return total;
-    }
+	public double sair() {
+
+	}
+
+	public double totalArrecadado() {
+
+	}
+
+	public double arrecadadoNoMes(int mes) {
+
+	}
+
+	public int totalDeUsos() {
+
+	}
+
 }

bc20cf0af0a1449cfef514e28450c780d42e3646
Enrique Silva <130712280+InvalidUsernameII@users.noreply.github.com>
2023-10-25 09:20:08 -0300

    Update Vaga.java
    Assim como especificado no quadro do projeto, a inclusão do método setDisponivel (boolean) foi feita.

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -33,4 +33,9 @@ public boolean disponivel() {
 	return disponivel; // Retorna o estado atual da vaga.
 }
 
+// Método que altera o estado da vaga de acordo com o parâmetro.
+public void setDisponivel(boolean disponivel) {
+	this.disponivel = disponivel; // Atribui o valor do parâmetro ao atributo disponivel.
+}
+
 }

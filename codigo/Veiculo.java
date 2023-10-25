763a0d3025d2d31cd202523e17e0cc0d5ce353ad
joaovitorbarrel <130581220+joaovitorbarrel@users.noreply.github.com>
2023-10-25 11:16:18 -0300

    Criação de exceções para os métodos da classe Veiculo.java

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -7,16 +7,17 @@ public class Veiculo {
         this.usos = new UsoDeVaga[500];
     }
 
-    public void estacionar(Vaga vaga) {
+    public void estacionar(Vaga vaga) throws VeiculoException {
         for (int i = 0; i < usos.length; i++) {
             if (usos[i] == null) {
                 usos[i] = new UsoDeVaga(vaga);
-                break;
+                return; // Estacionamento bem-sucedido, retornar sem exceção
             }
         }
+        throw new VeiculoException("Não foi possível estacionar. Não há vagas disponíveis.");
     }
 
-    public double sair() {
+    public double sair() throws VeiculoException {
         double total = 0;
         for (int i = 0; i < usos.length; i++) {
             if (usos[i] != null) {
@@ -24,35 +25,48 @@ public class Veiculo {
                 usos[i] = null;
             }
         }
-        return total;
+        
+        if (total == 0) {
+            throw new VeiculoException("O veículo não foi registrado no estacionamento." )
+        }
     }
 
-    public double totalArrecadado() {
+    public double totalArrecadado() throws VeiculoException {
         double total = 0;
         for (int i = 0; i < usos.length; i++) {
             if (usos[i] != null) {
                 total += usos[i].getVaga().getValorPago();
             }
         }
-        return total;
+        if (total == 0) {
+            throw new VeiculoException("Não foi arrecadado nenhum valor.")
+        }
     }
 
-    public double arrecadadoNoMes(int mes) {
+    public double arrecadadoNoMes(int mes) throws VeiculoException {
         double total = 0;
         for (int i = 0; i < usos.length; i++) {
             if (usos[i] != null && usos[i].getVaga().getMes() == mes) {
                 total += usos[i].getVaga().getValorPago();
             }
         }
-        return total;
+        
+        if (total == 0) {
+            throw new VeiculoException("Não foi arrecadado nenhum valor.")
+        }
     }
 
-    public int totalDeUsos() {
+    public int totalDeUsos() throws VeiculoException {
         int total = 0;
         for (int i = 0; i < usos.length; i++) {
             if (usos[i] != null) {
                 total++;
             }
         }
-        return total;
+        
+        if (total == 0) {
+            throw new VeiculoException("Não houve casos de uso do veículo.")
+        }
     }
+
+}

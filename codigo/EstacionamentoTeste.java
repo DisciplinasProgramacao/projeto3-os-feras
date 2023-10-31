cd27c6f405aacb0355ac26661b0784a353fe2d62
FelipeNeb <122832893+FelipeNeb@users.noreply.github.com>
2023-10-30 22:11:51 -0300

    Update EstacionamentoTeste.java

--- codigo/EstacionamentoTeste.java
+++ codigo/EstacionamentoTeste.java
@@ -1,98 +1,235 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 public class EstacionamentoTeste {
-    private Estacionamento estacionamento;
+    private Estacionamento estacionamento; 
+    @Before
+    public void setUp(){
+        Estacionamento estacionamento = new Estacionamento("Meu Estacionamento",5,5);
+    }
 
-    @BeforeEach
-    public void setUp() {
-        estacionamento = criarEstacionamentoDeExemplo();
+    @Test
+    public void testarConstrutor(){
+        assertEquals("Meu Estacionamento", estacionamento.getNome());
+        assertEquals(5, estacionamento.getQuantFileiras());
+        assertEquals(5, estacionamento.getVagasPorFileira());
+    }
+
+    @Test
+    public void testAddVeiculo() {
+        Cliente cliente = new Cliente("Cliente1", "ID1"); // Crie um cliente
+        Veiculo veiculo = new Veiculo("ABC123"); // Crie um veículo
+
+        try {
+            estacionamento.addCliente(cliente); // Adicione o cliente ao estacionamento
+            estacionamento.addVeiculo("ABC123", "ID1"); // Adicione o veículo ao cliente no estacionamento
+            assertTrue(cliente.possuiVeiculo("ABC123")); // Verifique se o veículo foi adicionado corretamente ao cliente
+        } catch (ExcecaoVeiculoJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 
     @Test
     public void testAddCliente() {
-        int tamanhoInicial = estacionamento.getClientes().size();
-        estacionamento.addCliente("Novo Cliente", "ID3");
-        int tamanhoFinal = estacionamento.getClientes().size();
-        assertEquals(tamanhoInicial + 1, tamanhoFinal);
+        Cliente cliente = new Cliente("Cliente1", "ID1"); // Crie um cliente
+
+        try {
+            estacionamento.addCliente(cliente); // Adicione o cliente ao estacionamento
+            assertTrue(estacionamento.getId().contains(cliente)); // Verifique se o cliente foi adicionado corretamente ao estacionamento
+        } catch (ExcecaoClienteJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 
     @Test
-    public void testAddVagas() {
-        int tamanhoInicial = estacionamento.getVagas().size();
-        estacionamento.addVagas("Vaga3", true);
-        int tamanhoFinal = estacionamento.getVagas().size();
-        assertEquals(tamanhoInicial + 1, tamanhoFinal);
+    public void testGerarVagas() {
+        Vaga[] vagas = estacionamento.getVagas();
+        int expectedTotalVagas = 5 * 15; // 5 fileiras com 5 vagas por fileira
+
+        assertEquals(expectedTotalVagas, vagas.length); // Verifica se o número total de vagas está correto
+
+        // Verifica se as vagas foram criadas com os valores esperados
+        int fila = 0;
+        int numero = 1;
+        for (Vaga vaga : vagas) {
+            char filaEsperada = (char) ('A' + fila);
+            int numeroEsperado = numero;
+
+            assertEquals(filaEsperada, vaga.getFila());
+            assertEquals(numeroEsperado, vaga.getNumero());
+
+            // Atualiza os valores esperados para a próxima vaga
+            if (numero < 5) {
+                numero++;
+            } else {
+                fila++;
+                numero = 1;
+            }
+        }
+
+        @Test
+    public void testValorMedioPorUso() {
+        Cliente cliente1 = new Cliente("Cliente1", "ID1");
+        Cliente cliente2 = new Cliente("Cliente2", "ID2");
+
+        // Adicione alguns usos para os clientes
+        cliente1.addVeiculo(new Veiculo("ABC123"));
+        cliente1.addVeiculo(new Veiculo("XYZ789"));
+        cliente2.addVeiculo(new Veiculo("DEF456"));
+
+        // Simule valores arrecadados pelos clientes
+        cliente1.getVeiculo("ABC123").sair("ABC123");
+        cliente1.getVeiculo("XYZ789").sair("XYZ789");
+        cliente2.getVeiculo("DEF456").sair("DEF456");
+
+        try {
+            estacionamento.addCliente(cliente1);
+            estacionamento.addCliente(cliente2);
+
+            double valorMedioEsperado = (cliente1.arrecadadoTotal() + cliente2.arrecadadoTotal()) / (cliente1.totalDeUsos() + cliente2.totalDeUsos());
+            assertEquals(valorMedioEsperado, estacionamento.valorMedioPorUso(), 0.01); // Use delta para lidar com arredondamento
+        } catch (ExcecaoClienteJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
+    }
+    
+    @Test
+    public void testTop5Clientes() {
+        Cliente cliente1 = new Cliente("Cliente1", "ID1");
+        Cliente cliente2 = new Cliente("Cliente2", "ID2");
+        Cliente cliente3 = new Cliente("Cliente3", "ID3");
+        Cliente cliente4 = new Cliente("Cliente4", "ID4");
+        Cliente cliente5 = new Cliente("Cliente5", "ID5");
+
+        // Adicione alguns usos para os clientes em diferentes meses
+        cliente1.addVeiculo(new Veiculo("ABC123"));
+        cliente2.addVeiculo(new Veiculo("XYZ789"));
+        cliente3.addVeiculo(new Veiculo("DEF456"));
+        cliente4.addVeiculo(new Veiculo("GHI789"));
+        cliente5.addVeiculo(new Veiculo("JKL012"));
+
+        // Simule valores arrecadados pelos clientes em diferentes meses
+        cliente1.getVeiculo("ABC123").sair("ABC123"); // Mês 1
+        cliente2.getVeiculo("XYZ789").sair("XYZ789"); // Mês 2
+        cliente3.getVeiculo("DEF456").sair("DEF456"); // Mês 3
+        cliente4.getVeiculo("GHI789").sair("GHI789"); // Mês 4
+        cliente5.getVeiculo("JKL012").sair("JKL012"); // Mês 5
+
+        try {
+            estacionamento.addCliente(cliente1);
+            estacionamento.addCliente(cliente2);
+            estacionamento.addCliente(cliente3);
+            estacionamento.addCliente(cliente4);
+            estacionamento.addCliente(cliente5);
+
+            // Verifique se o método top5Clientes retorna os nomes esperados com base na arrecadação no mês 3
+            assertEquals("[Cliente3, Cliente2, Cliente1, Cliente4, Cliente5]", estacionamento.top5Clientes(3));
+        } catch (ExcecaoClienteJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 
     @Test
     public void testEstacionar() {
-        Estacionamento status = estacionamento.estacionar("ID1");
-        assertEquals(Estacionamento.EstacionamentoStatus.SUCCESS, status);
+        Cliente cliente = new Cliente("Cliente1", "ID1");
+        Veiculo veiculo = new Veiculo("ABC123");
 
-        // Teste de estacionar em vaga ocupada
-        estacionamento.estacionar("ID2"); // Estaciona em outra vaga
-        status = estacionamento.estacionar("ID1"); // Tenta estacionar novamente em ID1
-        assertEquals(Estacionamento.EstacionamentoStatus.VAGA_OCUPADA, status);
+        try {
+            estacionamento.addCliente(cliente);
+            cliente.addVeiculo(veiculo);
 
-        // Teste de estacionar veículo não encontrado
-        status = estacionamento.estacionar("ID3");
-        assertEquals(Estacionamento.EstacionamentoStatus.VEICULO_NAO_ENCONTRADO, status);
+            // Simule a disponibilidade de uma vaga
+            estacionamento.getVagas()[0].setDisponivel(true);
 
-        // Teste de estacionar sem vagas disponíveis
-        estacionamento.getVagas().forEach(vaga -> vaga.setDisponivel(false));
-        status = estacionamento.estacionar("ID1");
-        assertEquals(Estacionamento.EstacionamentoStatus.SEM_VAGAS_DISPONIVEIS, status);
+            // Estacione o veículo
+            estacionamento.estacionar("ABC123");
+
+            // Verifique se a vaga foi ocupada corretamente
+            assertFalse(estacionamento.getVagas()[0].disponivel());
+        } catch (ExcecaoClienteJaCadastrado | ExcecaoVeiculoJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 
     @Test
     public void testSair() {
-        double valorPago = estacionamento.sair("ID1");
-        assertTrue(valorPago >= 0);
+        Cliente cliente = new Cliente("Cliente1", "ID1");
+        Veiculo veiculo = new Veiculo("ABC123");
 
-        // Teste de sair veículo não encontrado
-        valorPago = estacionamento.sair("ID3");
-        assertEquals(-1.0, valorPago);
+        try {
+            estacionamento.addCliente(cliente);
+            cliente.addVeiculo(veiculo);
+
+            // Simule a ocupação de uma vaga
+            estacionamento.getVagas()[0].setDisponivel(false);
+
+            // Simule a saída do veículo
+            double valorSaida = estacionamento.sair("ABC123");
+
+            // Verifique se a vaga está disponível
+            assertTrue(estacionamento.getVagas()[0].disponivel());
+
+            // Verifique se o valor de saída é maior que zero (indicando que houve cobrança)
+            assertTrue(valorSaida > 0);
+        } catch (ExcecaoClienteJaCadastrado | ExcecaoVeiculoJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 
     @Test
     public void testTotalArrecadado() {
-        double totalArrecadado = estacionamento.totalArrecadado();
-        assertTrue(totalArrecadado >= 0);
+        Cliente cliente1 = new Cliente("Cliente1", "ID1");
+        Cliente cliente2 = new Cliente("Cliente2", "ID2");
+        Veiculo veiculo1 = new Veiculo("ABC123");
+        Veiculo veiculo2 = new Veiculo("XYZ789");
+
+        try {
+            estacionamento.addCliente(cliente1);
+            estacionamento.addCliente(cliente2);
+            cliente1.addVeiculo(veiculo1);
+            cliente2.addVeiculo(veiculo2);
+
+            // Simule a entrada e saída de veículos e cobrança
+            estacionamento.estacionar("ABC123");
+            estacionamento.estacionar("XYZ789");
+            estacionamento.sair("ABC123");
+            estacionamento.sair("XYZ789");
+
+            // Verifique se o total arrecadado corresponde às cobranças
+            assertEquals(veiculo1.totalArrecadado() + veiculo2.totalArrecadado(), estacionamento.totalArrecadado(), 0.01);
+        } catch (ExcecaoClienteJaCadastrado | ExcecaoVeiculoJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 
     @Test
     public void testArrecadacaoNoMes() {
-        double arrecadacaoNoMes = estacionamento.arrecadacaoNoMes(10);
-        assertTrue(arrecadacaoNoMes >= 0);
-    }
+        Cliente cliente1 = new Cliente("Cliente1", "ID1");
+        Cliente cliente2 = new Cliente("Cliente2", "ID2");
+        Veiculo veiculo1 = new Veiculo("ABC123");
+        Veiculo veiculo2 = new Veiculo("XYZ789");
 
-    @Test
-    public void testValorMedioPorUso() {
-        double valorMedio = estacionamento.valorMedioPorUso();
-        assertTrue(valorMedio >= -1.0); // Pode ser -1.0 se não houver usos
-    }
+        try {
+            estacionamento.addCliente(cliente1);
+            estacionamento.addCliente(cliente2);
+            cliente1.addVeiculo(veiculo1);
+            cliente2.addVeiculo(veiculo2);
 
-    @Test
-    public void testTop5Clientes() {
-        String top5Clientes = estacionamento.top5Clientes(10);
-        assertNotNull(top5Clientes);
-    }
+            // Simule a entrada e saída de veículos e cobrança
+            estacionamento.estacionar("ABC123");
+            estacionamento.estacionar("XYZ789");
+            estacionamento.sair("ABC123");
+            estacionamento.sair("XYZ789");
 
-    private Estacionamento criarEstacionamentoDeExemplo() {
-        Cliente[] clientes = new Cliente[2];
-        clientes[0] = new Cliente("Cliente1", "ID1");
-        clientes[1] = new Cliente("Cliente2", "ID2");
+            // Defina o mês de referência para a arrecadação
+            int mes = 10;
 
-        List<Vaga> vagas = new ArrayList<>();
-        Vaga vaga1 = new Vaga("Vaga1", true);
-        Vaga vaga2 = new Vaga("Vaga2", true);
-        vagas.add(vaga1);
-        vagas.add(vaga2);
-
-        return new Estacionamento("Meu Estacionamento", clientes, vagas, 2, 2);
+            // Verifique se a arrecadação no mês corresponde às cobranças
+            assertEquals(veiculo1.arrecadadoNoMes(mes) + veiculo2.arrecadadoNoMes(mes), estacionamento.arrecadacaoNoMes(mes), 0.01);
+        } catch (ExcecaoClienteJaCadastrado | ExcecaoVeiculoJaCadastrado e) {
+            fail("Exceção inesperada: " + e.getMessage());
+        }
     }
 }

bde8fa029ae15ffad3caedea6c640c0452ff13df
joaovitorbarrel <130581220+joaovitorbarrel@users.noreply.github.com>
2023-11-01 11:49:55 -0300

    Atualização da classe Main #22

--- codigo/Main.java
+++ codigo/Main.java
@@ -2,41 +2,63 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); // Criar um objeto Scanner para ler a entrada do usuário
-		System.out.println("Bem-vindo ao app de estacionamento!"); // Imprimir uma mensagem de boas-vindas
-		System.out.println("Escolha uma das opções abaixo:"); // Imprimir as opções disponíveis
-		System.out.println("1 - Estacionamento");
-		System.out.println("2 - Vaga");
-		System.out.println("3 - Veiculo");
-		System.out.println("4 - Sair");
-		int opcao = sc.nextInt(); // Ler a opção escolhida pelo usuário
-		while (opcao != 4) { // Enquanto a opção não for 4 (sair)
-			switch (opcao) { // Usar um switch para executar a ação correspondente à opção
-				case 1: // Se a opção for 1 (Estacionamento)
-					TryQuest<Estacionamento> questEst = new TryQuest<Estacionamento>(); // Criar um objeto TryQuest para a classe Estacionamento
-					questEst.run(); // Executar o método run do objeto TryQuest
-					break;
-				case 2: // Se a opção for 2 (Vaga)
-					TryQuest<Vaga> questVaga = new TryQuest<Vaga>(); // Criar um objeto TryQuest para a classe Vaga
-					questVaga.run(); // Executar o método run do objeto TryQuest
-					break;
-				case 3: // Se a opção for 3 (Veiculo)
-					TryQuest<Veiculo> questVeic = new TryQuest<Veiculo>(); // Criar um objeto TryQuest para a classe Veiculo
-					questVeic.run(); // Executar o método run do objeto TryQuest
-					break;
-				default: // Se a opção for inválida
-					System.out.println("Opção inválida. Tente novamente."); // Imprimir uma mensagem de erro
-			}
-			System.out.println("Escolha uma das opções abaixo:"); // Imprimir novamente as opções disponíveis
-			System.out.println("1 - Estacionamento");
-			System.out.println("2 - Vaga");
-			System.out.println("3 - Veiculo");
-			System.out.println("4 - Sair");
-			opcao = sc.nextInt(); // Ler novamente a opção escolhida pelo usuário
-		}
-		sc.close(); // Fechar o objeto Scanner
-		System.out.println("Obrigado por usar o app de estacionamento!"); // Imprimir uma mensagem de despedida
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem-vindo ao app de estacionamento!");
+        System.out.println("Escolha uma das opções abaixo:");
+        System.out.println("1 - Estacionamento");
+        System.out.println("2 - Vaga");
+        System.out.println("3 - Veiculo");
+        System.out.println("4 - Histórico de Veiculo");
+        System.out.println("5 - Sair");
+        
+        Estacionamento estacionamento = new Estacionamento(); // Criar um objeto de estacionamento
 
+        int opcao = sc.nextInt();
+        while (opcao != 5) {
+            switch (opcao) {
+                case 1:
+                    TryQuest<Estacionamento> questEst = new TryQuest<Estacionamento>();
+                    questEst.run();
+                    break;
+                case 2:
+                    TryQuest<Vaga> questVaga = new TryQuest<Vaga>();
+                    questVaga.run();
+                    break;
+                case 3:
+                    TryQuest<Veiculo> questVeic = new TryQuest<Veiculo>();
+                    questVeic.run();
+                    break;
+                case 4:
+                    System.out.println("Digite a placa do veículo para ver o histórico:");
+                    String placa = sc.next();
+                    Veiculo veiculo = estacionamento.buscarVeiculo(placa);
+                    if (veiculo != null) {
+                        System.out.println("Histórico do Veículo " + placa);
+                        for (UsoDeVaga uso : veiculo.getHistoricoVagas()) {
+                            if (uso.getVaga() != null) {
+                                System.out.println("Data de entrada: " + uso.getDataEntrada());
+                                System.out.println("Data de saída: " + uso.getDataSaida());
+                                System.out.println("Valor Pago: " + uso.getVaga().getValorPago());
+                            }
+                        }
+                    } else {
+                        System.out.println("Veículo não encontrado.");
+                    }
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+            
+            System.out.println("Escolha uma das opções abaixo:");
+            System.out.println("1 - Estacionamento");
+            System.out.println("2 - Vaga");
+            System.out.println("3 - Veiculo");
+            System.out.println("4 - Histórico de Veiculo");
+            System.out.println("5 - Sair");
+            opcao = sc.nextInt();
+        }
+        sc.close();
+        System.out.println("Obrigado por usar o app de estacionamento!");
+    }
 }

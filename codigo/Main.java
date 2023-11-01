e62250bbe750e4e0094e06b6ace40c02fd68bd0c
SthelTorres <130579600+SthelTorres@users.noreply.github.com>
2023-11-01 12:02:07 -0300

    update classe main para master

--- codigo/Main.java
+++ codigo/Main.java
@@ -3,62 +3,105 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Bem-vindo ao app de estacionamento!");
-        System.out.println("Escolha uma das opções abaixo:");
-        System.out.println("1 - Estacionamento");
-        System.out.println("2 - Vaga");
-        System.out.println("3 - Veiculo");
-        System.out.println("4 - Histórico de Veiculo");
-        System.out.println("5 - Sair");
-        
-        Estacionamento estacionamento = new Estacionamento(); // Criar um objeto de estacionamento
+        Scanner scanner = new Scanner(System.in);
+        Estacionamento estacionamento = new Estacionamento("Estacionamento ABC", 5, 10);
 
-        int opcao = sc.nextInt();
-        while (opcao != 5) {
-            switch (opcao) {
+        System.out.println("Bem-vindo ao " + estacionamento.getNome() + "!");
+        int escolha;
+
+        do {
+            System.out.println("Escolha a ação desejada:");
+            System.out.println("1 - Entrar no estacionamento");
+            System.out.println("2 - Sair do estacionamento");
+            System.out.println("3 - Consultar vagas disponíveis");
+            System.out.println("4 - Consultar valor da hora");
+            System.out.println("5 - Sair do app");
+
+            escolha = scanner.nextInt();
+            scanner.nextLine(); // Limpar a quebra de linha após a leitura do número
+
+            switch (escolha) {
                 case 1:
-                    TryQuest<Estacionamento> questEst = new TryQuest<Estacionamento>();
-                    questEst.run();
+                    entradaEstacionamento(scanner, estacionamento);
                     break;
+
                 case 2:
-                    TryQuest<Vaga> questVaga = new TryQuest<Vaga>();
-                    questVaga.run();
+                    saidaEstacionamento(scanner, estacionamento);
                     break;
+
                 case 3:
-                    TryQuest<Veiculo> questVeic = new TryQuest<Veiculo>();
-                    questVeic.run();
+                    consultarVagasDisponiveis(estacionamento);
                     break;
+
                 case 4:
-                    System.out.println("Digite a placa do veículo para ver o histórico:");
-                    String placa = sc.next();
-                    Veiculo veiculo = estacionamento.buscarVeiculo(placa);
-                    if (veiculo != null) {
-                        System.out.println("Histórico do Veículo " + placa);
-                        for (UsoDeVaga uso : veiculo.getHistoricoVagas()) {
-                            if (uso.getVaga() != null) {
-                                System.out.println("Data de entrada: " + uso.getDataEntrada());
-                                System.out.println("Data de saída: " + uso.getDataSaida());
-                                System.out.println("Valor Pago: " + uso.getVaga().getValorPago());
-                            }
-                        }
-                    } else {
-                        System.out.println("Veículo não encontrado.");
-                    }
+                    consultarValorHora(scanner, estacionamento);
                     break;
+
+                case 5:
+                    System.out.println("Obrigado por usar o app de estacionamento!");
+                    break;
+
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    System.out.println("Escolha inválida. Tente novamente.");
+                    break;
             }
-            
-            System.out.println("Escolha uma das opções abaixo:");
-            System.out.println("1 - Estacionamento");
-            System.out.println("2 - Vaga");
-            System.out.println("3 - Veiculo");
-            System.out.println("4 - Histórico de Veiculo");
-            System.out.println("5 - Sair");
-            opcao = sc.nextInt();
+        } while (escolha != 5);
+
+        scanner.close();
+    }
+
+    private static void entradaEstacionamento(Scanner scanner, Estacionamento estacionamento) {
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Digite a placa do veículo:");
+        String placaVeiculo = scanner.nextLine();
+        System.out.println("Digite o tipo do veículo:");
+        String tipoVeiculo = scanner.nextLine();
+
+        try {
+            Veiculo veiculo = new Veiculo(placaVeiculo, tipoVeiculo);
+            Cliente cliente = new Cliente(nomeCliente, veiculo);
+
+            if (estacionamento.estacionar(cliente)) {
+                System.out.println("Cliente estacionado com sucesso!");
+            } else {
+                System.out.println("Estacionamento lotado. Não é possível estacionar.");
+            }
+        } catch (ExcecaoVeiculoJaCadastrado e) {
+            System.out.println(e.getMessage());
         }
-        sc.close();
-        System.out.println("Obrigado por usar o app de estacionamento!");
+    }
+
+    private static void saidaEstacionamento(Scanner scanner, Estacionamento estacionamento) {
+        System.out.println("Digite a placa do veículo:");
+        String placaVeiculo = scanner.nextLine();
+        estacionamento.sair(placaVeiculo);
+             System.out.println("O veículo com a placa " + placaVeiculo + " não está no estacionamento.");
+         }
+    }
+
+    private static void consultarVagasDisponiveis(Estacionamento estacionamento) {
+        int vagasDisponiveis = estacionamento.getVagasDisponiveis();
+        System.out.println("O estacionamento tem " + vagasDisponiveis + " vagas disponíveis.");
+    }
+
+    private static void consultarValorHora(Scanner scanner, Estacionamento estacionamento) {
+        System.out.println("Digite o id da vaga que você deseja consultar, ou deixe em branco para " +
+                "consultar o valor da hora do estacionamento:");
+        String idVaga = scanner.nextLine();
+
+        if (idVaga.isEmpty()) {
+            double valorHora = estacionamento.getValorHora();
+            System.out.println("O valor da hora do estacionamento é R$ " + valorHora + ".");
+        } else {
+            Vaga vaga = estacionamento.buscarVaga(idVaga);
+
+            if (vaga != null) {
+                double valorHora = vaga.getValorHora();
+                System.out.println("O valor da hora da vaga " + idVaga + " é R$ " + valorHora + ".");
+            } else {
+                System.out.println("A vaga " + idVaga + " não existe no estacionamento.");
+            }
+        }
     }
 }

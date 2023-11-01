7129682e6ac9a5f19145eb4785d6fafd52420a9a
Enrique Silva <130712280+InvalidUsernameII@users.noreply.github.com>
2023-11-01 08:04:05 -0300

    Update Vaga.java
    Correção da classe vaga. Por algum motivo, existiam duplicações que eram inexistentes no código original, acredito ter sido causado pelo merge que teve diversos conflitos. 
    
    Classe corrigida.

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -1,8 +1,5 @@
 public class Vaga{
-public class Vaga{
 
-private String id;
-private boolean disponivel;
 private String id;
 private boolean disponivel;
 
@@ -11,10 +8,11 @@ public Vaga(int fila, int numero) {
 	this.id = "F" + fila + "N" + numero;
 	this.disponivel = true; 
 }
-public Vaga(int fila, int numero) {
-	// Construtor que gera um id a partir da fila e do número.
-	this.id = "F" + fila + "N" + numero;
-	this.disponivel = true; 
+
+public Vaga(String string) {
+}
+
+public Vaga() {
 }
 
 public boolean estacionar() {
@@ -26,25 +24,7 @@ public boolean estacionar() {
 		return false; 
 	}
 }
-public boolean estacionar() {
-	// Método que tenta estacionar um veículo na vaga.
-	if (this.disponivel) { 
-		this.disponivel = false; 
-		return true; 
-	} else { 
-		return false; 
-	}
-}
 
-public boolean sair() {
-	// Método que tenta sair de uma vaga ocupada.
-	if (!this.disponivel) { 
-		this.disponivel = true; 
-		return true; 
-	} else { 
-		return false; 
-	}
-}
 public boolean sair() {
 	// Método que tenta sair de uma vaga ocupada.
 	if (!this.disponivel) { 
@@ -57,6 +37,19 @@ public boolean sair() {
 
 public boolean disponivel() {
 	return disponivel; // Retorna o estado atual da vaga.
+}
+
+public boolean isOcupada() {
+    return true;
+}
+
+public void setOcupada(boolean b) {
+}
+
+
+// Método que altera o estado da vaga de acordo com o parâmetro.
+public void setDisponivel(boolean disponivel) {
+	this.disponivel = disponivel; // Atribui o valor do parâmetro ao atributo disponivel.
 }
 
 }

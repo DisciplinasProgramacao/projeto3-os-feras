67dcfcc8e4630e484d5dd04aaa3c7a573a6f3e73
SthelTorres <130579600+SthelTorres@users.noreply.github.com>
2023-11-01 11:45:12 -0300

    correção classe main

--- codigo/Main.java
+++ codigo/Main.java
@@ -22,60 +22,19 @@ public class Main {
 
             switch (escolha) {
                 case 1:
-                    System.out.println("Digite o nome do cliente:");
-                    String nomeCliente = scanner.nextLine();
-                    System.out.println("Digite a placa do veículo:");
-                    String placaVeiculo = scanner.nextLine();
-                    System.out.println("Digite o tipo do veículo:");
-                    String tipoVeiculo = scanner.nextLine();
-
-                    Veiculo veiculo = new Veiculo(placaVeiculo, tipoVeiculo);
-                    Cliente cliente = new Cliente(nomeCliente, veiculo);
-
-                    if (estacionamento.estacionar(cliente)) {
-                        System.out.println("Cliente estacionado com sucesso!");
-                    } else {
-                        System.out.println("Estacionamento lotado. Não é possível estacionar.");
-                    }
+                    entradaEstacionamento(scanner, estacionamento);
                     break;
 
                 case 2:
-                    System.out.println("Digite o nome do cliente:");
-                    nomeCliente = scanner.nextLine();
-                    Cliente clienteSaida = estacionamento.buscarCliente(nomeCliente);
-
-                    if (clienteSaida != null) {
-                        double valor = estacionamento.sair(clienteSaida);
-                        System.out.println("Cliente " + nomeCliente + " saiu do estacionamento com placa " +
-                                clienteSaida.getVeiculo().getPlaca() + ". O valor a ser pago é R$ " + valor + ".");
-                    } else {
-                        System.out.println("O cliente " + nomeCliente + " não está no estacionamento.");
-                    }
+                    saidaEstacionamento(scanner, estacionamento);
                     break;
 
                 case 3:
-                    int vagasDisponiveis = estacionamento.getVagasDisponiveis();
-                    System.out.println("O estacionamento tem " + vagasDisponiveis + " vagas disponíveis.");
+                    consultarVagasDisponiveis(estacionamento);
                     break;
 
                 case 4:
-                    System.out.println("Digite o id da vaga que você deseja consultar, ou deixe em branco para " +
-                            "consultar o valor da hora do estacionamento:");
-                    String idVaga = scanner.nextLine();
-
-                    if (idVaga.isEmpty()) {
-                        double valorHora = estacionamento.getValorHora();
-                        System.out.println("O valor da hora do estacionamento é R$ " + valorHora + ".");
-                    } else {
-                        Vaga vaga = estacionamento.buscarVaga(idVaga);
-
-                        if (vaga != null) {
-                            double valorHora = vaga.getValorHora();
-                            System.out.println("O valor da hora da vaga " + idVaga + " é R$ " + valorHora + ".");
-                        } else {
-                            System.out.println("A vaga " + idVaga + " não existe no estacionamento.");
-                        }
-                    }
+                    consultarValorHora(scanner, estacionamento);
                     break;
 
                 case 5:
@@ -89,5 +48,67 @@ public class Main {
         } while (escolha != 5);
 
         scanner.close();
+    }
+
+    private static void entradaEstacionamento(Scanner scanner, Estacionamento estacionamento) {
+        System.out.println("Digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Digite a placa do veículo:");
+        String placaVeiculo = scanner.nextLine();
+        System.out.println("Digite o tipo do veículo:");
+        String tipoVeiculo = scanner.nextLine();
+
+        try {
+            Veiculo veiculo = new Veiculo(placaVeiculo, tipoVeiculo);
+            Cliente cliente = new Cliente(nomeCliente, veiculo);
+
+            if (estacionamento.estacionar(cliente)) {
+                System.out.println("Cliente estacionado com sucesso!");
+            } else {
+                System.out.println("Estacionamento lotado. Não é possível estacionar.");
+            }
+        } catch (ExcecaoVeiculoJaCadastrado e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void saidaEstacionamento(Scanner scanner, Estacionamento estacionamento) {
+        System.out.println("Digite a placa do veículo:");
+        String placaVeiculo = scanner.nextLine();
+        Cliente clienteSaida = estacionamento.buscarClientePorPlaca(placaVeiculo);
+
+        if (clienteSaida != null) {
+            Veiculo veiculoCliente = clienteSaida.getVeiculoPorPlaca(placaVeiculo);
+            double valor = estacionamento.sair(veiculoCliente.getPlaca());
+            System.out.println("Cliente " + clienteSaida.getNome() + " saiu do estacionamento com placa " +
+                    veiculoCliente.getPlaca() + ". O valor a ser pago é R$ " + valor + ".");
+        } else {
+            System.out.println("O veículo com a placa " + placaVeiculo + " não está no estacionamento.");
+        }
+    }
+
+    private static void consultarVagasDisponiveis(Estacionamento estacionamento) {
+        int vagasDisponiveis = estacionamento.getVagasDisponiveis();
+        System.out.println("O estacionamento tem " + vagasDisponiveis + " vagas disponíveis.");
+    }
+
+    private static void consultarValorHora(Scanner scanner, Estacionamento estacionamento) {
+        System.out.println("Digite o id da vaga que você deseja consultar, ou deixe em branco para " +
+                "consultar o valor da hora do estacionamento:");
+        String idVaga = scanner.nextLine();
+
+        if (idVaga.isEmpty()) {
+            double valorHora = estacionamento.getValorHora();
+            System.out.println("O valor da hora do estacionamento é R$ " + valorHora + ".");
+        } else {
+            Vaga vaga = estacionamento.buscarVaga(idVaga);
+
+            if (vaga != null) {
+                double valorHora = vaga.getValorHora();
+                System.out.println("O valor da hora da vaga " + idVaga + " é R$ " + valorHora + ".");
+            } else {
+                System.out.println("A vaga " + idVaga + " não existe no estacionamento.");
+            }
+        }
     }
 }

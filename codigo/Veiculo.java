8b0c2616659bb10da8b920d2463f7952864537ea
joaovitorbarrel <130581220+joaovitorbarrel@users.noreply.github.com>
2023-11-21 17:08:32 -0300

    Update Veiculo.java #27

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.io.Serializable;
 
-public class Veiculo {
+public class Veiculo implements Serializable {
     private String placa;
-    private List<UsoDeVaga> historicoVagas; // Lista para armazenar o histórico
+    private List<UsoDeVaga> historicoVagas;
     private Date dataEntrada;
 
     public Veiculo(String placa) {
@@ -13,38 +15,32 @@ public class Veiculo {
         this.dataEntrada = new Date();
     }
 
-    public void estacionar(Vaga vaga) throws VeiculoException {
-        for (UsoDeVaga uso : historicoVagas) {
-            if (uso.getVaga() == null) {
-                uso.setVaga(vaga);
-                uso.setDataEntrada(new Date());
-                return; // Estacionamento bem-sucedido, retornar sem exceção
-            }
-        }
-        throw new VeiculoException("Não foi possível estacionar. Não há vagas disponíveis.");
+   public void estacionar(Vaga vaga) {
+        UsoDeVaga uso = new UsoDeVaga(vaga);
+        usos.add(uso);
     }
 
     public double sair() {
-        double total = 0;
+        double totalArrecadado = 0.0;
         Date dataSaida = new Date();
 
-        for (UsoDeVaga uso : historicoVagas) {
+        for (UsoDeVaga uso : usos) {
             Vaga vaga = uso.getVaga();
             if (vaga != null) {
-                vaga.setValorPago(0); // Zerar o valor da vaga após a saída
-                uso.setDataSaida(dataSaida); // Registrar a data de saída
-                total += vaga.getValorPago();
+                vaga.setValorPago(0);
+                uso.setDataSaida(dataSaida);
+                totalArrecadado += vaga.getValorPago();
             }
         }
 
-        historicoVagas.clear(); // Limpar o histórico de vagas
+        usos.clear();
 
-        return total;
+        return totalArrecadado;
     }
 
     public double totalArrecadado() {
-        double total = 0;
-        for (UsoDeVaga uso : historicoVagas) {
+        double total = 0.0;
+        for (UsoDeVaga uso : usos) {
             Vaga vaga = uso.getVaga();
             if (vaga != null) {
                 total += vaga.getValorPago();
@@ -54,8 +50,8 @@ public class Veiculo {
     }
 
     public double arrecadadoNoMes(int mes) {
-        double total = 0;
-        for (UsoDeVaga uso : historicoVagas) {
+        double total = 0.0;
+        for (UsoDeVaga uso : usos) {
             Vaga vaga = uso.getVaga();
             if (vaga != null && vaga.getMes() == mes) {
                 total += vaga.getValorPago();
@@ -65,6 +61,22 @@ public class Veiculo {
     }
 
     public int totalDeUsos() {
-        return historicoVagas.size();
+        return usos.size();
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+    
+    public List<UsoDeVaga> gerarRelatorio(boolean ordenarPorData) {
+        List<UsoDeVaga> relatorio = new ArrayList<>(historicoVagas);
+
+        if (ordenarPorData) {
+            relatorio.sort(Comparator.comparing(UsoDeVaga::getDataEntrada));
+        } else {
+            relatorio.sort(Comparator.comparing(UsoDeVaga::getValorPago).reversed());
+        }
+
+        return relatorio;
     }
 }

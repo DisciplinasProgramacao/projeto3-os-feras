7cf615543a3a3a5161a037713163b420c6f99576
Enrique Silva <130712280+InvalidUsernameII@users.noreply.github.com>
2023-11-21 22:48:30 -0300

    Update Cliente.java

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -6,12 +6,22 @@ public class Cliente {
     private String id;
     private ArrayList<Veiculo> veiculos;
     private List<String> historico;
+    private String categoria;
+    private Turno turno;
+    private static final double MENSALIDADE_TURNO = 200.0;
+    private static final double MENSALIDADE_MENSALISTA = 500.0;
 
-    public Cliente(String nome, String id) {
+    public Cliente(String nome, String id, String categoria) {
         this.nome = nome;
         this.id = id;
+        this.categoria = categoria;
         this.veiculos = new ArrayList<>();
         this.historico = new ArrayList<>();
+    }
+
+    public Cliente(String nome, String id, String categoria, Turno turno) {
+        this(nome, id, categoria);
+        this.turno = turno;
     }
 
     public void addVeiculo(Veiculo veiculo) {
@@ -66,4 +76,43 @@ public class Cliente {
     public List<String> getHistorico() {
         return historico;
     }
+
+
+
+    public double getMensalidade() {
+        if (this.categoria.equals("turno")) {
+            return MENSALIDADE_TURNO;
+        } else if (this.categoria.equals("mensalista")) {
+            return MENSALIDADE_MENSALISTA;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public Turno getTurno() {
+        return this.turno;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ArrayList<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public Veiculo getVeiculo(String placa) {
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo.getPlaca().equals(placa)) {
+                return veiculo;
+            }
+        }
+        return null;
+    }
+
+
 }
